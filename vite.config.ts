e45ddd3ec3b3d8de175a9514b86414a2ead80121
giamyiweijeremystudy <giamyiweijeremy.study@gmail.com>
2026-04-01 11:25:14 +0800

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: process.env.GITHUB_PAGES_BASE ?? './',
+    base: process.env.NODE_ENV === 'production' ? '/QuizMaker/' : './',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
